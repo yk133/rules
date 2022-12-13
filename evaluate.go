6cda62a3b337498620fab42bcc7ec94cc12137b9
yk133 <1060682190@qq.com>
2022-12-13 13:57:36 +0800

    add with error

--- evaluate.go
+++ evaluate.go
@@ -9,3 +9,16 @@ func Evaluate(rule string, items map[string]interface{}) (bool, error) {
 	}
 	return ev.Process(items)
 }
+
+func EvaluateWithErrInfo(rule string, items map[string]interface{}) (bool, error) {
+	ev, err := parser.NewEvaluator(rule)
+	if err != nil {
+		return false, err
+	}
+	result, err := ev.Process(items)
+	if err != nil {
+		return result, err
+	} else {
+		return result, ev.LastDebugErr()
+	}
+}
