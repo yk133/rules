d075c1446e8b71ab95ddd18f8f6c0ec2512b145f
yk133 <1060682190@qq.com>
2022-12-14 16:07:30 +0800

    return  debug error

--- evaluate.go
+++ evaluate.go
@@ -10,15 +10,15 @@ func Evaluate(rule string, items map[string]interface{}) (bool, error) {
 	return ev.Process(items)
 }
 
-func EvaluateWithErrInfo(rule string, items map[string]interface{}) (bool, error) {
+func EvaluateWithErrInfo(rule string, items map[string]interface{}) (ok bool, err error, debugErr error) {
 	ev, err := parser.NewEvaluator(rule)
 	if err != nil {
-		return false, err
+		return false, err, nil
 	}
 	result, err := ev.Process(items)
 	if err != nil {
-		return result, err
+		return result, err, nil
 	} else {
-		return result, ev.LastDebugErr()
+		return result, nil, ev.LastDebugErr()
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20220911224424-aa1f1f12a846
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/nikunjy/rules v1.2.0
 	github.com/stretchr/testify v1.3.0
 )
 
